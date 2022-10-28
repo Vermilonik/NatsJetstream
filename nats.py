f92086a3529c46f450edd9d384b285a1bb3607d2
Alexander <73836577+Vermilonik@users.noreply.github.com>
2022-10-28 19:56:46 +0300

    Add files via upload

---
+++ nats.py
@@ -0,0 +1,67 @@
+import asyncio
+from typing import Optional
+
+import msgpack
+import nats
+from aiogram import Bot, Dispatcher, types
+from aiogram.exceptions import TelegramRetryAfter
+from aiogram.filters.command import Command
+from command_mk2 import CommandMk2
+from nats import errors
+
+bot = Bot(token="")
+admin_id =
+dp = Dispatcher()
+
+
+@dp.message(CommandMk2("write {num} {msg}"))
+async def writer(m: types.Message, num: Optional[str], msg: Optional[str]):
+    print(num)
+    print(msg)
+    nc = await nats.connect("localhost:4222")
+    js = nc.jetstream()
+
+    for i in range(0, int(num)):
+        a = msgpack.packb(msg)
+        if m.document:
+            ab = msgpack.packb(m.document.file_id)
+            ack2 = await js.publish("mass_bullshit", ab)
+            print(ack2)
+        ack = await js.publish("mass_bullshit", a)
+        print(a)
+        print(ack)
+
+    await nc.close()
+
+
+@dp.message(Command(commands=["listen"]))
+async def listener(m: types.Message, batch_size: int = 5, polling_timeout=10)
+    nc = await nats.connect("localhost:4222")
+    js = nc.jetstream()
+    psub = await js.pull_subscribe(subject=">", durable="aiogram")
+    if not psub:
+        raise ValueError("No consumer subscription, pizdec")
+    while True:
+        try:
+            msgs = await psub.fetch(batch_size)
+            for msg in msgs:
+                print(msg)
+                print(msgpack.unpackb(msg.data))
+                try:
+                    await bot.send_message(admin_id, msgpack.unpackb(msg.data))
+                except aiogram.exceptions.TelegramForbiddenError:
+                    pass
+                await msg.ack()
+        except errors.TimeoutError:
+            await m.answer("Рассылка закончена")
+            break
+        except TelegramRetryAfter as e:
+            await asyncio.sleep(float(e.retry_after))
+            continue
+
+
+async def aiogram_bot():
+    await asyncio.gather(dp.start_polling(bot))
+
+if __name__ == "__main__":
+    asyncio.run(aiogram_bot())
